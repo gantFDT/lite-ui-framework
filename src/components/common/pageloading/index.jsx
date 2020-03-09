363f9553d6d09e4a9917190c8531fa93f436c497
张洋瑞 <zhangdousang@yeah.net>
2020-03-09 17:29:38 +0800

    refactor(pageloading): 替换PageLoading->Loading

--- src/components/common/pageloading/index.jsx
+++ src/components/common/pageloading/index.jsx
@@ -1,12 +1,12 @@
 // https://umijs.org/plugin/umi-plugin-react.html#dynamicimport
 import React from 'react';
 import { Spin,Icon } from 'antd';
-import {PageLoading} from 'gantd'
+import { Loading } from 'gantd'
 
 
 //设置全局loading样式
 Spin.setDefaultIndicator(<Icon type="loading-3-quarters" style={{ fontSize: 24 }} spin />)
-const Loading = () => (
+const PageLoading = () => (
   // <div
   //   style={{
   //     paddingTop: 100,
@@ -14,9 +14,9 @@ const Loading = () => (
   //   }}
   // >
   //   <Spin size="large" />
-  //   <PageLoading index={2} height={150}/>
+  //   <Loading index={2} height={150}/>
   // </div>
-  <PageLoading index={2} height='calc(100vh - 40px)'/>
+  <Loading index={2} height='calc(100vh - 40px)'/>
 );
 
-export default Loading;
+export default PageLoading;
